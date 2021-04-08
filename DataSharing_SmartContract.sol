fce7f1c03b4777291c5c7a5a2cea3a772547f9d0
Zhan, Jennifer <zhanr@rpi.edu>
2021-04-08 20:23:12 +0800

    debug Clause1; implement Clause4 and part of Clause5

--- DataSharing_SmartContract.sol
+++ DataSharing_SmartContract.sol
@@ -1,22 +1,29 @@
+pragma solidity ^0.4.0;
 contract Covid19usecase{
-    //participating entities with Ethereum addresses
     address public data_contributor; 
     address public user; 
     enum contractState { 
-        NotReady, Created, ReadyforRequireRequest, ReadyforSubmitRequest, ReadyforReview, Active, Inactive, Aborted, Terminate
+        NotReady, Created, ReadyforRequireRequest, ReadyforSubmitRequest, ReadyforReview, Active, Inactive, Aborted, Terminate, Expire
     }   
     contractState public state; 
     uint startTime;
     uint daysAfter;
     int request_approval_result;
+    uint remainTime;
+    Contributor[] public contributor;
     
+    struct Contributor {
+        string _firstName;
+        string _lastName;
+    }
     
     //contructor
-    function Covid19usecase(){
+    constructor() public{
         startTime = block.timestamp;
-        data_contributor = 0x583031d1113ad414f02576bd6afabfb302140225;//address of data_contributor
-        user = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
-        state = contractState.Inactive;
+        daysAfter = 1825; //5 years
+        data_contributor = msg.sender;//address of sender
+        user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        state = contractState.NotReady;
         request_approval_result = 0;
     }
     
@@ -35,40 +42,72 @@ contract Covid19usecase{
     }
     
     event RequestApprovalDone(string msg);//to announce result if the request submitted by users could be approved.
+    //event ShowRemainingTime(uint remainingTime); 
     
+    function addcontributor(string memory _firstName, string memory _lastName) public onlyContributor{
+        contributor.push(Contributor(_firstName,_lastName));
+    }
     
-    function CreateContract() onlyContributor {
+    //Clause1
+    //For each proposed Research Project, User(s) agree(s) to submit a Data Use Request to the Data Access Committee 
+    //for review and approval to access the Data.
+    function CreateContract() public onlyContributor {
         require(state == contractState.NotReady);
-            state = contractState.ReadyforRequireRequest;
-            
+            state = contractState.ReadyforRequireRequest; //once locked the container will do a self check on the sensors
+            //RequireRequest(msg.sender); //trigger event
     }
     
     
-    function RequireRequest() onlyContributor {
+    function RequireRequest() public onlyContributor {
         require(state == contractState.ReadyforRequireRequest);
-            state = contractState.ReadyforSubmitRequest; 
+            state = contractState.ReadyforSubmitRequest; //once locked the container will do a self check on the sensors
             
     }
     
-    function UserSubmitRequest() onlyUser {
+    function UserSubmitRequest() public onlyUser {
         require(state == contractState.ReadyforSubmitRequest);
             state = contractState.ReadyforReview;
     }
     
-    function ApproveRequest(int result) onlyContributor {
+    function ApproveRequest(int result) public onlyContributor {
         require(state == contractState.ReadyforReview);
         request_approval_result = result;
-        if(request_approval_result == 1){//indicating the request has been approved
+        if(request_approval_result == 1){ //request is approved.
             state = contractState.Active;
-            RequestApprovalDone("Request is Approved");//trigger event with result
+            emit RequestApprovalDone("Request is Approved");
         }
-        else if(request_approval_result == 0){
+        else if(request_approval_result == 0){ //request is not qpproved.
             state = contractState.Aborted;
-            RequestApprovalDone("Contract Aborted: Failure."); //trigger event with result
-            selfdestruct(msg.sender);
+            emit RequestApprovalDone("Contract Aborted: Failure.");
+             selfdestruct(msg.sender);
         }
             
-    } 
+    }
+    
+    //Clause4
+    //The Data Use Request will remain in effect for a period of five (5) years from the Data Use Request Effective Date 
+    //and will automatically expire at the end of this period unless terminated or renewed.
+    function terminateRequest() public {
+        require(state == contractState.Active);
+           state = contractState.Terminate;
+           
+    }
+    
+    function renewed() public {
+        startTime=block.timestamp;
+    }
+    
+    function DetectValid() public {
+        require(state == contractState.Active);
+        if(block.timestamp > startTime + daysAfter * 1 days){
+            state = contractState.Expire;
+        }
+    }
+    
+    function ShowRemainingTime() public {
+        require(state == contractState.Active);
+          remainTime=(daysAfter * 1 days)-(block.timestamp-startTime);
+    }
     
     
 }
